d00b34716b5a835df3227154d86cd38a174743a1
singalMorning <452976339@qq.com>
2020-08-09 19:00:55 +0800

    week_02 assignment

---
+++ Week_02/[144]二叉树的前序遍历.py
@@ -0,0 +1,41 @@
+# 给定一个二叉树，返回它的 前序 遍历。 
+# 
+#  示例: 
+# 
+#  输入: [1,null,2,3]  
+#    1
+#     \
+#      2
+#     /
+#    3 
+# 
+# 输出: [1,2,3]
+#  
+# 
+#  进阶: 递归算法很简单，你可以通过迭代算法完成吗？ 
+#  Related Topics 栈 树 
+#  👍 334 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+
+        def helper(root):
+            if not root: return
+            res.append(root.val)
+            helper(root.left)
+            helper(root.right)
+
+        helper(root)
+        return res
+
+# leetcode submit region end(Prohibit modification and deletion)
---
+++ Week_02/[94]二叉树的中序遍历.py
@@ -0,0 +1,40 @@
+# 给定一个二叉树，返回它的中序 遍历。 
+# 
+#  示例: 
+# 
+#  输入: [1,null,2,3]
+#    1
+#     \
+#      2
+#     /
+#    3
+# 
+# 输出: [1,3,2] 
+# 
+#  进阶: 递归算法很简单，你可以通过迭代算法完成吗？ 
+#  Related Topics 栈 树 哈希表 
+#  👍 616 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+
+        def helper(root):
+            if not root:
+                return
+            helper(root.left)
+            res.append(root.val)
+            helper(root.right)
+
+        helper(root)
+        return res
+# leetcode submit region end(Prohibit modification and deletion)
